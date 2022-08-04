f610ed9f7bfb58d0b9187c456898835f5b2669b6
João <santoscostaj808@gmail.com>
2022-08-04 01:16:22 -0300

    tentativa

--- back-end/Bloco-27-Programacao-Orientada-a-Objetos-POO-e-SOLID/dia-01-Introducao-a-Orientacao-a-Objetos/src/index.ts
+++ back-end/Bloco-27-Programacao-Orientada-a-Objetos-POO-e-SOLID/dia-01-Introducao-a-Orientacao-a-Objetos/src/index.ts
@@ -15,7 +15,7 @@ class Person {
   }
 }
 
-const p1 = new Person("Maria", 171, 58);
+const p1 = new Person("tores", 171, 58);
 const p2 = new Person("João", 175, 66);
 console.log(p1.name, p1.height, p1.weight);
 console.log(p2.name, p2.height, p2.weight);
