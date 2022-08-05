d5bd2fcd076fe7b3369867cbaba4c8b540ac25f5
João <santoscostaj808@gmail.com>
2022-08-05 00:15:45 -0300

    class tv criada

--- back-end/Bloco-27-Programacao-Orientada-a-Objetos-POO-e-SOLID/dia-01-Introducao-a-Orientacao-a-Objetos/src/index.ts
+++ back-end/Bloco-27-Programacao-Orientada-a-Objetos-POO-e-SOLID/dia-01-Introducao-a-Orientacao-a-Objetos/src/index.ts
@@ -21,3 +21,32 @@ console.log(p1.name, p1.height, p1.weight);
 console.log(p2.name, p2.height, p2.weight);
 p1.sleep();
 p2.sleep();
+
+class Tv {
+  private _brand: string;
+  private _size: number;
+  private _resolution: string;
+  private _connections: string[];
+  private _connectedTo?: string;
+
+  constructor(
+    brand: string,
+    size: number,
+    resolution: string,
+    connections: string[]
+  ) {
+    this._size = size;
+    this._resolution = resolution;
+    this._connections = connections;
+    this._brand = brand;
+  }
+
+  public tornOn(): void {
+    console.log(
+      `Tv ${this._brand}, ${this._size}", resolution ${this._resolution} \navailable connections: ${this._connections}`
+    );
+  }
+}
+
+const tv1 = new Tv("LG", 62, "4K", ["HDMI", "Ethernet", "WI-FI"]);
+tv1.tornOn();
