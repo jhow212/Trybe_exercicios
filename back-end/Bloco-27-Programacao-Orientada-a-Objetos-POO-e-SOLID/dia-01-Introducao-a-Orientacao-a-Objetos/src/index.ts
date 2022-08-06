7f7b304aa1d72d0e9ce9f2e92b52259cb3d70b0a
João <santoscostaj808@gmail.com>
2022-08-05 22:46:44 -0300

    criando a class tv e melhorando a class person para fixar

--- back-end/Bloco-27-Programacao-Orientada-a-Objetos-POO-e-SOLID/dia-01-Introducao-a-Orientacao-a-Objetos/src/index.ts
+++ back-end/Bloco-27-Programacao-Orientada-a-Objetos-POO-e-SOLID/dia-01-Introducao-a-Orientacao-a-Objetos/src/index.ts
@@ -1,13 +1,32 @@
 class Person {
   public name: string;
-  public height: number;
-  public weight: number;
+  readonly height: number;
+  private _weight: number;
+  private _age: number;
 
-  constructor(n: string, h: number, w: number) {
-    console.log(`Creating person ${n}`);
-    this.name = n;
-    this.height = h;
-    this.weight = w;
+  constructor(name: string, height: number, weight: number, age: number) {
+    console.log(`Creating person ${name}`);
+    this.name = name;
+    this.height = height;
+    this._weight = weight;
+    this._age = age;
+  }
+  getWeight(): number {
+    return this._weight;
+  }
+
+  get age(): number {
+    return this._age;
+  }
+
+  set age(newValue: number) {
+    if (newValue >= 0 && newValue < 200) {
+      this._age;
+    }
+  }
+
+  birthday(): void {
+    this._age += 1;
   }
 
   sleep(): void {
@@ -15,12 +34,38 @@ class Person {
   }
 }
 
-const p1 = new Person("tores", 171, 58);
-const p2 = new Person("João", 175, 66);
-console.log(p1.name, p1.height, p1.weight);
-console.log(p2.name, p2.height, p2.weight);
-p1.sleep();
-p2.sleep();
+const p1 = new Person("tores", 171, 58, 19);
+const p2 = new Person("João", 175, 66, 18);
+
+//Alterando diretamente o name que é publico
+p1.name = "Mariah";
+console.log(p1.name);
+p2.name = "João Vitor";
+console.log(p2.name);
+
+//Acessando o metodo publico que manipula um atributo privado
+console.log(`${p1.name}: ${p1.getWeight()}`);
+console.log(`${p2.name}: ${p2.getWeight()}`);
+
+//Acessando o metodo com getter para manipular um atributo privado como se fosse privado
+console.log(`${p1.name}: ${p1.age}`);
+console.log(`${p2.name}: ${p2.age}`);
+
+//Acessando um metodo publico que manipula um objeto privado
+p1.birthday();
+console.log(`${p1.name}: ${p1.age}`);
+p2.birthday();
+console.log(`${p2.name}: ${p2.age}`);
+
+//Acessando um metodo setter para manipular um atributo privado como se fosse publico
+p1.age = 17;
+console.log(`${p1.name}: ${p1.age}`);
+p2.age = 18;
+console.log(`${p2.name}: ${p2.age}`);
+
+//Leitura de atributo readonly
+console.log(`${p1.name}: ${p1.height}`);
+console.log(`${p2.name}: ${p2.height}`);
 
 class Tv {
   private _brand: string;
