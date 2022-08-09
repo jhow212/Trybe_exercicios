7fe21a5fc558a2043cc23cbf381b13b888e16faa
João <santoscostaj808@gmail.com>
2022-08-09 17:04:03 -0300

    criando classe de email e classe para mecher com o array de email

---
+++ back-end/Bloco-27-Programacao-Orientada-a-Objetos-POO-e-SOLID/dia-01-Introducao-a-Orientacao-a-Objetos/src/classe.lista.email.ts
@@ -0,0 +1,31 @@
+import Email from "./classe.email";
+
+export default class MailList {
+  constructor(private mailList: Email[] = []) {}
+
+  get all(): Email[] {
+    return this.mailList;
+  }
+
+  getEmailsSentBy(mailAddress: string): Email[] {
+    return this.mailList.filter((mail) => mail.from === mailAddress);
+  }
+
+  getEmailsTo(mailAddress: string): Email[] {
+    return this.mailList.filter((mail) => mail.to === mailAddress);
+  }
+
+  getEmailsBySubject(searchString: string): Email[] {
+    return this.mailList.filter(
+      (mail) => mail.subject.indexOf(searchString) !== -1
+    );
+  }
+
+  addEmail(newMail: Email): void {
+    this.mailList.push(newMail);
+  }
+
+  removeEmail(mailToRemove: Email): void {
+    this.mailList = this.mailList.filter((mail) => mail !== mailToRemove);
+  }
+}
