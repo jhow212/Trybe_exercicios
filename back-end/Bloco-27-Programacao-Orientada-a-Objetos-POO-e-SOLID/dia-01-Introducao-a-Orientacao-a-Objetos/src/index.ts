7ebf41e1b174227cc0c31a70bf93bd18b967c048
João <santoscostaj808@gmail.com>
2022-08-10 19:33:17 -0300

    fix: descomentando codigos de exemplo

--- back-end/Bloco-27-Programacao-Orientada-a-Objetos-POO-e-SOLID/dia-01-Introducao-a-Orientacao-a-Objetos/src/index.ts
+++ back-end/Bloco-27-Programacao-Orientada-a-Objetos-POO-e-SOLID/dia-01-Introducao-a-Orientacao-a-Objetos/src/index.ts
@@ -1,43 +1,43 @@
 import Email from "./classe.email";
 import MailList from "./classe.lista.email";
-// import Person from "./classe.person";
-// import Tv from "./classe.tv";
+import Person from "./classe.person";
+import Tv from "./classe.tv";
 
-// const p1 = new Person("tores", 171, 58, 19);
-// const p2 = new Person("João", 175, 66, 18);
+const p1 = new Person("tores", 171, 58, 19);
+const p2 = new Person("João", 175, 66, 18);
 
-// //Alterando diretamente o name que é publico
-// p1.name = "Mariah";
-// console.log(p1.name);
-// p2.name = "João Vitor";
-// console.log(p2.name);
+//Alterando diretamente o name que é publico
+p1.name = "Mariah";
+console.log(p1.name);
+p2.name = "João Vitor";
+console.log(p2.name);
 
-// //Acessando o metodo publico que manipula um atributo privado
-// console.log(`${p1.name}: ${p1.getWeight()}`);
-// console.log(`${p2.name}: ${p2.getWeight()}`);
+//Acessando o metodo publico que manipula um atributo privado
+console.log(`${p1.name}: ${p1.getWeight()}`);
+console.log(`${p2.name}: ${p2.getWeight()}`);
 
-// //Acessando o metodo com getter para manipular um atributo privado como se fosse privado
-// console.log(`${p1.name}: ${p1.age}`);
-// console.log(`${p2.name}: ${p2.age}`);
+//Acessando o metodo com getter para manipular um atributo privado como se fosse privado
+console.log(`${p1.name}: ${p1.age}`);
+console.log(`${p2.name}: ${p2.age}`);
 
-// //Acessando um metodo publico que manipula um objeto privado
-// p1.birthday();
-// console.log(`${p1.name}: ${p1.age}`);
-// p2.birthday();
-// console.log(`${p2.name}: ${p2.age}`);
+//Acessando um metodo publico que manipula um objeto privado
+p1.birthday();
+console.log(`${p1.name}: ${p1.age}`);
+p2.birthday();
+console.log(`${p2.name}: ${p2.age}`);
 
-// //Acessando um metodo setter para manipular um atributo privado como se fosse publico
-// p1.age = 17;
-// console.log(`${p1.name}: ${p1.age}`);
-// p2.age = 18;
-// console.log(`${p2.name}: ${p2.age}`);
+//Acessando um metodo setter para manipular um atributo privado como se fosse publico
+p1.age = 17;
+console.log(`${p1.name}: ${p1.age}`);
+p2.age = 18;
+console.log(`${p2.name}: ${p2.age}`);
 
-// //Leitura de atributo readonly
-// console.log(`${p1.name}: ${p1.height}`);
-// console.log(`${p2.name}: ${p2.height}`);
+//Leitura de atributo readonly
+console.log(`${p1.name}: ${p1.height}`);
+console.log(`${p2.name}: ${p2.height}`);
 
-// const tv1 = new Tv("LG", 62, "4K", ["HDMI", "Ethernet", "WI-FI"]);
-// tv1.tornOn();
+const tv1 = new Tv("LG", 62, "4K", ["HDMI", "Ethernet", "WI-FI"]);
+tv1.tornOn();
 
 //Testando a classe Email e a classe que controla o Array de emails
 const friendEmail = "friend@personalmail.com";
